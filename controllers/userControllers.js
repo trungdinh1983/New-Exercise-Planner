879286e028b6486972b046612838d608793d7332
YOUR NAMETrung <trungdinh1983@gmail.com>
2023-09-08 18:19:33 -0700

    Update User Controller

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,9 +1,9 @@
-const { User } = require("../models"); // Import User model
-const bcrypt = require("bcrypt"); // For password hashing
-const passport = require("passport"); // For authentication
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+const passport = require("passport");
 
 // Create a new user
-const createUser = async (req, res, next) => {
+exports.createUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
     const hashedPassword = await bcrypt.hash(password, 10);
@@ -11,98 +11,68 @@ const createUser = async (req, res, next) => {
     const newUser = await User.create({
       name,
       email,
-      password_digest: hashedPassword,
+      password: hashedPassword,
     });
 
-    return res.status(201).json(newUser);
+    res.status(201).json(newUser);
   } catch (error) {
-    next(error);
+    res.status(500).json({ error: "Could not create user" });
   }
 };
 
-// Login
-const loginUser = (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) return next(err);
-    if (!user) return res.status(401).json(info);
+// Read user profile
+exports.getUserProfile = (req, res) => {
+  res.status(200).json(req.user);
+};
 
+// Update user profile
+exports.updateUserProfile = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    await req.user.update({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(200).json(req.user);
+  } catch (error) {
+    res.status(500).json({ error: "Could not update user profile" });
+  }
+};
+
+// Delete user profile
+exports.deleteUserProfile = async (req, res) => {
+  try {
+    await req.user.destroy();
+    res.status(204).end();
+  } catch (error) {
+    res.status(500).json({ error: "Could not delete user profile" });
+  }
+};
+
+// User login
+exports.loginUser = (req, res, next) => {
+  passport.authenticate("local", (error, user) => {
+    if (error) {
+      return res.status(500).json({ error: "Login failed" });
+    }
+    if (!user) {
+      return res.status(401).json({ error: "Invalid credentials" });
+    }
     req.logIn(user, (err) => {
-      if (err) return next(err);
-      return res.status(200).json(user);
+      if (err) {
+        return res.status(500).json({ error: "Login failed" });
+      }
+      res.status(200).json({ message: "Login successful", user });
     });
   })(req, res, next);
 };
 
-// Logout
-const logoutUser = (req, res) => {
+// User logout
+exports.logoutUser = (req, res) => {
   req.logout();
-  res.status(200).json({ message: "Logged out" });
-};
-
-// Get all users (considering soft deletes)
-const getAllUsers = async (req, res, next) => {
-  try {
-    const users = await User.findAll({ where: { deletedAt: null } });
-    res.status(200).json(users);
-  } catch (error) {
-    next(error);
-  }
-};
-
-// Get a single user by ID (considering soft deletes)
-const getUserById = async (req, res, next) => {
-  try {
-    const user = await User.findOne({ where: { id: req.params.id, deletedAt: null } });
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
-  }
-};
-
-// Update a user
-const updateUser = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const { name, email } = req.body;
-    const user = await User.findByPk(id);
-
-    if (!user || user.deletedAt) return res.status(404).json({ message: "User not found" });
-
-    user.name = name;
-    user.email = email;
-    await user.save();
-
-    res.status(200).json(user);
-  } catch (error) {
-    next(error);
-  }
-};
-
-// Soft delete a user
-const deleteUser = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByPk(id);
-
-    if (!user) return res.status(404).json({ message: "User not found" });
-
-    user.deletedAt = new Date();
-    await user.save();
-
-    res.status(200).json({ message: "User soft deleted" });
-  } catch (error) {
-    next(error);
-  }
-};
-
-module.exports = {
-  createUser,
-  loginUser,
-  logoutUser,
-  getAllUsers,
-  getUserById,
-  updateUser,
-  deleteUser,
+  res.status(200).json({ message: "Logout successful" });
 };
